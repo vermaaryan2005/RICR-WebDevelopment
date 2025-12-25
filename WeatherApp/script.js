bbec1ca584e8fbeff31ebf093dc699cf144736dc
vermaaryan2005 <vermaaryan2005@gmail.com>
2025-12-25 11:08:34 +0530

    Add HTML structure and JavaScript functionality for FakeStore and WeatherApp projects

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,30 +1,36 @@
-async function getWeather(){
-    const city= document.getElementById("city").ariaValueMax.trim();
-    const {lat,lon} = await getGeoLoc(city);
+async function getWeather() {
+  const city = document.getElementById("city").value.trim();
+  const { lat, lon } = await getGeoLoc(city);
+  console.log(lat, lon);
 
-    const response= await fetch('https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=056a61d9d7f3343f0d63f9ddf98ce7bd');
-const data= await response.json();
-document.getElementById("weatherdata").innerHTML= '
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1cefe43006e662107c8d0454bde6852c`
+  );
+
+  const data = await response.json();
+
+  document.getElementById("WeatherData").innerHTML = `
            <div>
-            <><p>Temprature : ${(data.main.temp - 273.14).toFixed(2)}C</p>
+            <p>Temperature : ${(data.main.temp - 273.14).toFixed(2)}ºC</p>
             <p>Humidity : ${data.main.humidity}%</p>
-            <p>Description : ${data.weather[0].description}</p></>
-        /div>
-            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}
-            @4x.png" alt="WeatherIcon"/>
-';
+            <p>Description : ${data.weather[0].description}</p>
+          </div>
+          <img src="https://openweathermap.org/img/wn/${
+            data.weather[0].icon
+          }@4x.png" alt="WeatherIcon" />
+  `;
 }
-async function getGeoLoc(City){
-    console.log(City);
-    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=5&appid=056a61d9d7f3343f0d63f9ddf98ce7bd');
 
-    const data = await response.json();
+async function getGeoLoc(City) {
+  console.log(City);
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=1&appid=1cefe43006e662107c8d0454bde6852c`
+  );
 
-    const lat = data[0].lat;
-    const lon = data[0].lan; 
+  const data = await response.json();
 
-    return {lat,lon };
+  const lat = data[0].lat;
+  const lon = data[0].lon;
 
-
-
+  return { lat, lon };
 }
